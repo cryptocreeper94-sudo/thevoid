25ab461fb2877972b48bdca6266d52e3f20163ef
cryptocreeper94 <jason@livfitoken.com>
2026-03-25 00:21:37 -0500

    Add Trust Layer SSO consumer endpoints (trustLayerSSO.ts)

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
+import { registerTrustLayerSSO } from './trustLayerSSO';
 
 const app = express();
 const httpServer = createServer(app);
@@ -62,6 +63,8 @@ app.use((req, res, next) => {
 
 (async () => {
   await registerRoutes(httpServer, app);
+  // Trust Layer SSO consumer endpoints
+  registerTrustLayerSSO(app);
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
@@ -102,3 +105,4 @@ app.use((req, res, next) => {
     },
   );
 })();
+
